387cb3884c6345402e0f1a21644728f8c0e8ba5d
Juho Jeong <aspectran@gmail.com>
2025-03-06 14:08:42 +0900

    Update

--- src/main/java/app/root/common/listener/UserTrackingListener.java
+++ src/main/java/app/root/common/listener/UserTrackingListener.java
@@ -25,6 +25,8 @@ import com.aspectran.core.component.bean.annotation.Component;
 import com.aspectran.core.component.session.Session;
 import com.aspectran.core.component.session.SessionListener;
 import com.aspectran.core.component.session.SessionListenerRegistration;
+import com.aspectran.undertow.server.TowServer;
+import com.aspectran.undertow.support.SessionListenerRegistrationBean;
 import com.aspectran.utils.StringUtils;
 import com.aspectran.utils.annotation.jsr305.NonNull;
 
@@ -56,10 +58,15 @@ public class UserTrackingListener extends InstantActivitySupport implements Sess
 
     @Override
     public void initialize() throws Exception {
-        SessionListenerRegistration sessionListenerRegistration =
-                getBeanRegistry().getBean(SessionListenerRegistration.class);
-        if (sessionListenerRegistration == null) {
-            throw new IllegalStateException("Bean for SessionListenerRegistration must be defined");
+        SessionListenerRegistration sessionListenerRegistration;
+        if (getBeanRegistry().containsBean(SessionListenerRegistration.class)) {
+            sessionListenerRegistration = getBeanRegistry().getBean(SessionListenerRegistration.class);
+        } else {
+            if (getBeanRegistry().containsBean(TowServer.class)) {
+                sessionListenerRegistration = new SessionListenerRegistrationBean();
+            } else {
+                throw new IllegalStateException("Bean for SessionListenerRegistration must be defined");
+            }
         }
         sessionListenerRegistration.register(this, getActivityContext().getName());
     }

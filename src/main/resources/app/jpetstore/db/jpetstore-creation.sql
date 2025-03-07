b440bf1640228ba5c867095df3997e5abca883b5
Juho Jeong <aspectran@gmail.com>
2025-03-07 19:34:26 +0900

    Update

--- src/main/resources/app/jpetstore/db/jpetstore-creation.sql
+++ src/main/resources/app/jpetstore/db/jpetstore-creation.sql
@@ -1,3 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS jpetstore;
-RUNSCRIPT FROM 'classpath:app/jpetstore/mybatis/jpetstore-schema.sql';
-RUNSCRIPT FROM 'classpath:app/jpetstore/mybatis/jpetstore-dataload.sql';
+RUNSCRIPT FROM 'classpath:app/jpetstore/db/jpetstore-schema.sql';
+RUNSCRIPT FROM 'classpath:app/jpetstore/db/jpetstore-dataload.sql';
